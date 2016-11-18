f208e2951b77a5550b474194ebbb123cf246ba22
bcolem18 <bcolem18@uncc.edu>
2016-11-18 19:29:35 +0000

    Ne

--- Reduce.cpp
+++ Reduce.cpp
@@ -30,7 +30,7 @@ int findMin(int arr[], int low, int high)
     } else {
         // stop spawing and find the
         // minuimum
-        int minimum = 200000;
+        int minimum = 200;
         for(int i = low; i <= high; i++) {
             minimum = std::min(minimum, arr[i]);
         }
