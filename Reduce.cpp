3202a4201b238e0180d1159621c55454236249d9
bcolem18 <bcolem18@uncc.edu>
2016-11-18 19:08:58 +0000

    n

--- Reduce.cpp
+++ Reduce.cpp
@@ -104,6 +104,9 @@ start = std::chrono::system_clock::now();
     end = std::chrono::system_clock::now();
  std::chrono::duration<double> elapsed_seconds = end-start;
  std::time_t end_time = std::chrono::system_clock::to_time_t(end); 
+ 
+   std::cout << "finished acomputation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s\n"; 
     
     //return min;
     
